99b9f51ffb18f4eca3ecc8e3df7d96f2b6426985
magdalenahuget <magdalena.huget@gmail.com>
2022-11-17 11:16:59 +0100

    Initial commit with the content of the task

---
+++ data.js
@@ -0,0 +1,14 @@
+// Good or Awesome
+// Ask a person in the predefined function if the person feels "good" or "awesome"
+
+// Check the answer
+// Check the answer of a person. If the answer is neither "good" nor "awesome", console log that you do not understand the answer - else repeat the answer on the console
+
+// The age question
+// Write a function that asks a person for his age and returns it. Check the age and make different console logs for the ages smaller equal 5, bigger 5 and smaller equal 20, bigger 20 and smaller equal 40, and bigger 40 - e.g. if age is bigger 40 you could log: "Soon you will be in pension - great!"
+
+// Further conversation
+// Think about a question you would ask in a small talk conversation and let your program ask this question. For this purpose declare a function with a nice name. Return the answer of the user and react to it in a proper way.
+
+// Background Materials
+// Functions
