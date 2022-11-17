d449d49c5ef7cbe2b5cce22cdf2c70e3cceefad7
magdalenahuget <magdalena.huget@gmail.com>
2022-11-17 12:38:19 +0100

    Add solved tasks.

---
+++ README.md
@@ -0,0 +1,28 @@
+Simple calculator
+Implement a simple calulator which just calls function which does simple calculations and console logs the result.
+
+What are you going to learn?
+How to use functions
+Get to known to functions with return values
+Get to known to functions with parameters
+Tasks
+Create a "+" function
+Create a function addition which takes two parameters (numbers) and returns the sum of both
+
+Create a "-" function
+Create a function subtraction which takes two parameters (numbers) and returns the difference of both
+
+Create a "*" function
+Create a function multiplication which takes two parameters (numbers) and returns the product of both
+
+Create a "/" function
+Create a function division which takes two parameters (numbers) and returns the Quotient of both. Be aware that a division by zero should not be possible. If your function detects a divion by zero it should just return 0.
+
+Create a "%" function
+Create a function modulo which takes two parameters (numbers) and returns the result of their modulo equation.
+
+Uncomment code
+Uncomment implemented code after you have created your functions and check the results.
+
+Hint
+Modulo returns the rest of a division. 10%3 for example equals 1 since 10/3 = 3 and 1 is left.
--- data.js
+++ data.js
@@ -1,14 +1,99 @@
+const prompt = require("prompt-sync")();
+
+function sayHello(nameOfPerson)
+{
+    console.log(`Hello ${nameOfPerson}, how are you today, good or awesome?`);
+}
+
+sayHello("Friend");
+
+// I could call the parameter here also nameOfPerson, but I just wanted to
+// show you that the variable does not need to have the same name as in the
+// previous function - just give it a proper name
+
 // Good or Awesome
 // Ask a person in the predefined function if the person feels "good" or "awesome"
+
+function askHowPersonIs(nameOfAnyPerson)
+{
+    console.log(`${nameOfAnyPerson}, how are you today, good or awesome?`);
+}
+
+askHowPersonIs("Harry");
 
 // Check the answer
 // Check the answer of a person. If the answer is neither "good" nor "awesome", console log that you do not understand the answer - else repeat the answer on the console
 
+function getAnswerOfThePerson()
+{
+    let answer = prompt("How are you today, good or awesome?");
+    if (answer === "good" || answer === "awesome") {
+    // TODO: Get the answer of the person from the command line
+    // HINT: We learned in the previous chapter how to get input
+    // from the command line
+    return answer + " - I like Your enthusiasm";
+    } else {
+        return answer + " - I do not understand the answer."
+    }
+}
+
+let yourName = "";
+
+sayHello(yourName);
+askHowPersonIs(yourName);
+
+let answerOfThePerson = getAnswerOfThePerson();
+console.log(answerOfThePerson);
+
+// TODO: Check the answer of a person
+// If the answer is neither "good" nor "awesome"
+// console log that you do not understand the 
+// answer - else repeat the answer on the console
+
 // The age question
 // Write a function that asks a person for his age and returns it. Check the age and make different console logs for the ages smaller equal 5, bigger 5 and smaller equal 20, bigger 20 and smaller equal 40, and bigger 40 - e.g. if age is bigger 40 you could log: "Soon you will be in pension - great!"
+
+function ageQuestion() 
+{
+    let ageAnswer = prompt("How old are You?");
+    if (ageAnswer <= 5) {
+        console.log("You are so young!");
+    } else if (ageAnswer > 5 && ageAnswer <= 20)
+    {
+        console.log("This is Your best time!");
+    } else if (ageAnswer > 20 && ageAnswer <= 40)
+    {
+        console.log("You are an adult. Take Your Time");
+    } else if (ageAnswer > 40)
+    {
+        console.log("Soon you will be in pension - great!");
+    }
+};
+
+ageQuestion();
+
+// TODO: Write a function that asks a person for
+// his age and returns it. Check the age and make
+// different console logs for the ages smaller
+// equal 5, bigger 5 and smaller equal 20, bigger
+// 20 and smaller equal 40, and bigger 40 - e.g.
+// if age is bigger 40 you could print: "Soon you
+// will be in pension - great!"
 
 // Further conversation
 // Think about a question you would ask in a small talk conversation and let your program ask this question. For this purpose declare a function with a nice name. Return the answer of the user and react to it in a proper way.
 
-// Background Materials
-// Functions
+function codingLevelQuestion() 
+{
+    let codingLevelAnswer = prompt("What is your coding level? From 1 - 100.");
+    if (codingLevelAnswer <50 && codingLevelAnswer > -1) {
+        console.log("Keep working hard");
+    } else if (codingLevelAnswer >= 50 && codingLevelAnswer < 101)
+    {
+        console.log("Go on holiday!");
+    } else if ( codingLevelAnswer >100 || codingLevelAnswer < 0) {
+        console.log("Incorrect coding level");
+    }
+};
+
+codingLevelQuestion();
